92c9087ebea68fb9dd1c64378a794f0c67ba8f4e
leandromanciagli <leandromanciagli@gmail.com>
2022-01-30 19:05:26 -0300

    Inicialización del proyecto

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+// src/Controller/HomeController.php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomeController extends AbstractController
+{
+    /**
+    * @Route("/home", name="home")
+    */
+    public function number(): Response
+    {
+        $number = random_int(0, 100);
+
+        return $this->render('home.html.twig', ['numerito' => $number]);
+    }
+}

d98f333094223fad2104e5b5916cfd69234b8d09
leandromanciagli <leandromanciagli@gmail.com>
2022-01-30 19:31:22 -0300

    Se agrega la entidad Producto

---
+++ src/Entity/Producto.php
@@ -0,0 +1,37 @@
+<?php
+
+// src/Entity/Product.php
+namespace App\Entity;
+
+// use App\Repository\ProductRepository;
+// use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ProductRepository::class)
+ */
+class Product
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $price;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    // ... getter and setter methods
+}

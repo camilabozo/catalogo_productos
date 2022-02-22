9f0adc59a26da33128f982d6d9bae4642c61b330
Camila Bozo <bozo_cami@hotmail.com>
2022-02-22 15:04:41 -0300

    Se crea Home Controller. Se gestiona el login de usuarios. Se comienza a estilar los templates.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
 use App\Entity\Producto;
 
 class HomeController extends AbstractController
